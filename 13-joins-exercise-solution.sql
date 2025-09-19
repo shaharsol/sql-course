fb627bdfd9277c6d6a03e2ae6d08731112cd07ce
shaharsol <shahar@gnift.com>
2025-09-19 18:22:12 +0300

    join solutions

--- 13-joins-exercise-solution.sql
+++ 13-joins-exercise-solution.sql
@@ -6,6 +6,29 @@ ON		ctr.id = ord.countryId
 AND		ctr.name = 'USA';
 
 -- display all products ordered from USA
+SELECT 	DISTINCT prd.name
+FROM	orders as ord
+JOIN	countries as ctr
+ON		ctr.id = ord.countryId
+AND		ctr.name = 'USA'
+JOIN	`order-details` as detail
+ON		detail.orderId = ord.id
+JOIN	products as prd
+on		prd.id = detail.productId
+order by prd.name ASC;
 
--- generate a list of suppliers and the number of thier products we 
--- sold in USA, ordered from most popular to least
+-- generate a list of suppliers and the number of their products we 
+-- sold in USA, sorted from most popular to least
+SELECT 	sp.id, sp.companyName, sum(detail.quantity) as total_per_supplier
+FROM	orders as ord
+JOIN	countries as ctr
+ON		ctr.id = ord.countryId
+AND		ctr.name = 'USA'
+JOIN	`order-details` as detail
+ON		detail.orderId = ord.id
+JOIN	products as prd
+on		prd.id = detail.productId
+join	suppliers as sp
+on		prd.supplierId = sp.id
+group by sp.id
+order by total_per_supplier DESC;
