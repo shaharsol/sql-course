1d1de6323568bf7f5689999917538971a948cf19
shaharsol <shahar@gnift.com>
2025-09-20 13:11:45 +0300

    dml exercise

---
+++ 17-dml-exercise.sql
@@ -0,0 +1,10 @@
+-- congradulations! you were accepted to work in Northwind! add yourself to the employees table 
+-- (make sure to use REAL country and city)
+
+-- congradulations! you received an end-year bonus and can now move to live in Las Vegas, Nevada
+-- update your employee information in the database to reflect that
+
+-- delete all data from orders table
+
+-- bad news! your deletion of the orders table caused a company crisis and you are fired!
+-- delete yourself from the employees table
